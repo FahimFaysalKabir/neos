bab9808bd9f0a8e90d5dcbccf58befad5d5f39af
FahimFaysalKabir <badhon_121@yahoo.com>
2018-07-21 04:48:30 +0600

    header and footer.php created

--- index.php
+++ index.php
@@ -1,85 +1,4 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?php bloginfo('name');?>|<?php bloginfo('description');?> </title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-	<meta name="author" content="FREEHTML5.CO" />
-
-  <!--
-	//////////////////////////////////////////////////////
-
-	FREE HTML5 TEMPLATE
-	DESIGNED & DEVELOPED by FREEHTML5.CO
-
-	Website: 		http://freehtml5.co/
-	Email: 			info@freehtml5.co
-	Twitter: 		http://twitter.com/fh5co
-	Facebook: 		https://www.facebook.com/fh5co
-
-	//////////////////////////////////////////////////////
-	 -->
-
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-	<link rel="shortcut icon" href="favicon.ico">
-
-	<!-- <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'> -->
-
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="href="<?php bloginfo('template_url'); ?>/css/animate.css">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/icomoon.css">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css">
-	<!-- Owl Carousel -->
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/owl.carousel.min.css">
-	<link rel="stylesheet" href="/css/owl.theme.default.min.css">
-
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
-
-
-	<!-- Modernizr JS -->
-	<script src="<?php bloginfo('template_url'); ?>/js/modernizr-2.6.2.min.js"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="<?php bloginfo('template_url'); ?>/js/respond.min.js"></script>
-	<![endif]-->
-
-	</head>
-	<body>
-	<header>
-		<div class="container text-center">
-			<div class="fh5co-navbar-brand">
-				<a class="fh5co-logo" href="index.html">Neos</a>
-			</div>
-			<nav id="fh5co-main-nav" role="navigation">
-				<ul>
-					<li><a href="index.html" class="active">Home</a></li>
-					<li><a href="work.html">Work</a></li>
-					<li><a href="services.html">Services</a></li>
-					<li><a href="about.html">About</a></li>
-					<li><a href="contact.html">Contact</a></li>
-				</ul>
-			</nav>
-		</div>
-	</header>
+<?php get_header();?>
 
 	<div class="owl-carousel owl-carousel1 owl-carousel-fullwidth fh5co-light-arrow animate-box" data-animate-effect="fadeIn">
 		<div class="item"><a href="<?php echo get_template_directory_uri(); ?>/images/featured-1.jpg" class="image-popup"><img src="<?php echo get_template_directory_uri(); ?>/images/featured-1.jpg" alt="image"></a></div>
@@ -365,23 +284,7 @@
 			</div>
 		</div>
 	</div><!-- end: fh5co-parallax -->
-	<footer>
-		<div id="footer" class="fh5co-border-line">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8 col-md-offset-2 text-center">
-						<p>Copyright 2016 Free Html5 <a href="#">Neos</a>. All Rights Reserved. <br>Made with <i class="icon-heart3 love"></i> by <a href="http://freehtml5.co/" target="_blank">Freehtml5.co</a> / Demo Images: <a href="https://www.pexels.com/" target="_blank">Pexels</a> &amp; <a href="http://plmd.me/" target="_blank">PLMD</a> </p>
-						<p class="fh5co-social-icons">
-							<a href="#"><i class="icon-twitter-with-circle"></i></a>
-							<a href="#"><i class="icon-facebook-with-circle"></i></a>
-							<a href="#"><i class="icon-instagram-with-circle"></i></a>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
-
+<?php get_footer();?>
 	<!-- jQuery -->
 	<script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
 	<!-- jQuery Easing -->
